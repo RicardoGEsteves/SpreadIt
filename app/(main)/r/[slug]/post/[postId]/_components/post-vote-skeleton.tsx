3f2189dd6a56c179f2458dcb953123282af6663f
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-26 04:29:49 +0000

    feat: :sparkles: Type: Enhancement | Title Addition of Post Voting Functionality and Post Details
    Description:
    The changes introduce a new feature for voting on posts. Two new components have been added: PostVoteSkeleton and SubSpreadItPostPage. The PostVoteSkeleton component provides a skeleton loading screen for the voting feature, while the SubSpreadItPostPage component handles the logic for fetching and displaying post data, including the voting feature.
    
    Main Files Walkthrough:
    - app/(main)/r/[slug]/post/[postId]/_components/post-vote-skeleton.tsx: A new component PostVoteSkeleton has been added. This component displays a skeleton loading screen for the voting feature, including upvote and downvote buttons and a loading spinner for the score.
    - app/(main)/r/[slug]/post/[postId]/page.tsx: A new component SubSpreadItPostPage has been added. This component fetches post data from the database or cache, and displays the post details, including the voting feature. It uses the PostVoteSkeleton component as a fallback while the post data is being fetched. It also includes a Suspense component for handling asynchronous data fetching.
    
    Users can now see each post details in a dynamic post page.

---
+++ app/(main)/r/[slug]/post/[postId]/_components/post-vote-skeleton.tsx
@@ -0,0 +1,25 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
+
+const PostVoteSkeleton = () => {
+  return (
+    <div className="flex items-center flex-col pr-6 w-20">
+      {/* upvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="h-5 w-5 text-muted-foreground" />
+      </div>
+
+      {/* score */}
+      <div className="text-center py-2 font-medium text-sm text-primary">
+        <Loader2 className="h-3 w-3 animate-spin" />
+      </div>
+
+      {/* downvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="h-5 w-5 text-muted-foreground" />
+      </div>
+    </div>
+  );
+};
+
+export default PostVoteSkeleton;
---
+++ app/(main)/r/[slug]/post/[postId]/page.tsx
@@ -0,0 +1,87 @@
+import { notFound } from "next/navigation";
+import { Post, User, Vote } from "@prisma/client";
+import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
+
+import { db } from "@/lib/db";
+import { redis } from "@/lib/redis";
+import { CachedPost } from "@/types/redis";
+
+import PostVoteSkeleton from "./_components/post-vote-skeleton";
+import PostVoteServer from "@/components/posts/post-vote/server";
+import { formatTimeToNow } from "@/lib/utils";
+import TextEditorOutput from "@/components/rich-text-editor/text-editor-output";
+
+type SubSpreadItPostPageProps = {
+  params: {
+    postId: string;
+  };
+};
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+const SubSpreadItPostPage = async ({ params }: SubSpreadItPostPageProps) => {
+  const cachedPost = (await redis.hgetall(
+    `post:${params.postId}`
+  )) as CachedPost;
+
+  let post: (Post & { votes: Vote[]; author: User }) | null = null;
+
+  if (!cachedPost) {
+    post = await db.post.findFirst({
+      where: {
+        id: params.postId,
+      },
+      include: {
+        votes: true,
+        author: true,
+      },
+    });
+  }
+
+  if (!post && !cachedPost) return notFound();
+
+  return (
+    <div>
+      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
+        <Suspense fallback={<PostVoteSkeleton />}>
+          <PostVoteServer
+            postId={post?.id ?? cachedPost.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId,
+                },
+                include: {
+                  votes: true,
+                },
+              });
+            }}
+          />
+        </Suspense>
+
+        <div className="sm:w-0 w-full flex-1 bg-background p-4 rounded-sm">
+          <p className="max-h-40 mt-1 truncate text-xs text-emerald-500">
+            Posted by u/{post?.author.username ?? cachedPost.authorUsername}{" "}
+            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+          </p>
+          <h1 className="text-xl font-semibold py-2 leading-6 text-muted-foreground">
+            {post?.title ?? cachedPost.title}
+          </h1>
+
+          <TextEditorOutput content={post?.content ?? cachedPost.content} />
+          <Suspense
+            fallback={
+              <Loader2 className="h-5 w-5 animate-spin text-muted-foreground" />
+            }
+          >
+            {/* <CommentsSection postId={post?.id ?? cachedPost.id} /> */}
+          </Suspense>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubSpreadItPostPage;
